b6998d5820b089a86f7da0c767cb470551287d5f
ksy52081 <ksy52081@gmail.com>
2022-04-18 00:20:41 +0900

    [Lecture] #2.2 #2.3

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 alert("hi");
+const a = 5;
+const b = 2;
+let myName = "snagyoon"
 
+console.log(5+2);
+console.log(5-2);
+console.log(5*2);
+console.log(5/2);
+console.log("my name is "+ myName);
+
+myName = "Sangyoon Kim"
+console.log("my new name is "+ myName);
