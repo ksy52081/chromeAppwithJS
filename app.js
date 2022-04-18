d6601b4d00480d25de30ee657ae6b976ad63acda
ksy52081 <ksy52081@gmail.com>
2022-04-18 16:22:28 +0900

    [Lecture] #2.5 array

--- app.js
+++ app.js
@@ -11,3 +11,18 @@ console.log("my name is "+ myName);
 
 myName = "Sangyoon Kim"
 console.log("my new name is "+ myName);
+
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thr = "thr";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+const daysOfWeek = mon+tue+wed+thr+fri+sat+sun;
+const arrayDaysOfWeek = [mon,tue,wed,thr,fri,sat];
+console.log(arrayDaysOfWeek[4])
+arrayDaysOfWeek.push("sun")
+console.log(arrayDaysOfWeek[4])
+
+const nonsence = [1,"hello", false, null, true, undefined, "SY"];
