373e119e73cfe906d076310e17471447cf26887d
ksy52081 <ksy52081@gmail.com>
2022-04-23 00:30:46 +0900

    [Lecture] #3.1 JS에서 HTML의 element를 가져오고 수정할 수 있다_document getelementbyid

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-document.title = "Hello JS"
+const title = document.getElementById("title");
+
+console.dir(title);
+// console.log 가 아닌 
+// console.dir()의 경우 element를 더 자세히 보여준다. 
+
+title.innerText = "Got You! "
+//  js코드로 html 출력이 바뀜
