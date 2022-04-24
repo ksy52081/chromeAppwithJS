07cd2ec21e13d5ddb2e880df145351b63ac20eaa
ksy52081 <ksy52081@gmail.com>
2022-04-25 00:09:23 +0900

    [Lecture] #4.2 JS가 아닌 HTML에서 inputData 적합성검사하기 HTML submit 기능

--- app.js
+++ app.js
@@ -2,14 +2,7 @@ const loginInput = document.querySelector("#login-from input");
 const loginButton = document.querySelector("#login-from button");
 
 function onLoginBtnClick(){
-    // console.dir(loginInput);
     const username = loginInput.value;
-    if (username === ""){
-        alert("Plz write your name");
-    } else if (username.length > 15){
-        alert("your name is too long");
-
-    }
-    console.log("clicked!")
+    console.log(username);
 }
 loginButton.addEventListener("click", onLoginBtnClick);
