f9ab3d375bf48243e79942d7d8d5188c125a31f5
ksy52081 <ksy52081@gmail.com>
2022-04-25 10:16:09 +0900

    [Lecture] #4.2  addEventListner 함수의 첫번째 argument는 event 이고, event 안에 preventDefault함수가 있음

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+const loginForm = document.querySelector("#login-from");
 const loginInput = document.querySelector("#login-from input");
-const loginButton = document.querySelector("#login-from button");
 
-function onLoginBtnClick(){
+function onLoginSubmit(tomato){
+    tomato.preventDefault();
+
+    console.log(tomato);
+    // onLoginSubmit에 대한 argument를 받아온다!(?)
+    // onLoginSubmit에 대한 default 기본 행동을 발생하지 않도록 막는다
+
     const username = loginInput.value;
     console.log(username);
 }
-loginButton.addEventListener("click", onLoginBtnClick);
+
+// 이제는  click이 아닌 html의 submit을 감지해야 함다.
+loginForm.addEventListener("submit", onLoginSubmit);
