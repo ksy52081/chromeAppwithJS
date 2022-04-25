4ac6d179248f19b08f45752b45f1081e633eb261
ksy52081 <ksy52081@gmail.com>
2022-04-26 01:12:12 +0900

    [Lecture] #4.5 #4.6 조건에 따라 보여주고 싶은 레이아웃이 다를 때

--- app.js
+++ app.js
@@ -2,16 +2,33 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+}
 
 function onLoginSubmit(event){
     event.preventDefault();
-    const username = loginInput.Value;
-    localStorage.setItem("username", username);
+    const typedusername = loginInput.Value;
+    localStorage.setItem(USERNAME_KEY, typedusername);
     loginForm.classList.add(HIDDEN_CLASSNAME);
     // greeting.innerText = "Hello" + username;
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    paintGreetings(typedusername);
 } 
+
 
 // 이제는  click이 아닌 html의 submit을 감지해야 함다.
 loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+console.log(savedUsername);
+if(savedUsername === null){
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+    //show the greeting
+    paintGreetings(savedUsername);
+}
