5f0b7419701c4417854bfc65c259a96e31d384e7
ksy52081 <ksy52081@gmail.com>
2022-04-25 23:41:18 +0900

    [Lecture] #4.5 localStorage에 데이터 저장하는 방법

--- app.js
+++ app.js
@@ -6,23 +6,12 @@ const HIDDEN_CLASSNAME = "hidden"
 function onLoginSubmit(event){
     event.preventDefault();
     const username = loginInput.Value;
+    localStorage.setItem("username", username);
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    greeting.innerText = "Hello" + username;
+    // greeting.innerText = "Hello" + username;
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
-}
-
-const link = document.querySelector("a");
-
-function handleLinkClick(event){
-    event.preventDefault();
-    console.dir(event)
-    // alert("clicked")
-    // 앞서 이야기했듯, alert가 가동되면 alert가 종료되기 전까지 모든 행동이 멈춘다.
-}
-
-link.addEventListener("click", handleLinkClick)
-
+} 
 
 // 이제는  click이 아닌 html의 submit을 감지해야 함다.
 loginForm.addEventListener("submit", onLoginSubmit);
