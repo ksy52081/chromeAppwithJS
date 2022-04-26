3fff73aa697333b930ab0952562eef7de797f115
ksy52081 <ksy52081@gmail.com>
2022-04-26 19:24:47 +0900

    [Lecture] #5.2 #5.3 문자.padStart를 이용해 9:11:5 에서 09:11:05 꼴로 만들기

--- js/clock.js
+++ js/clock.js
@@ -2,11 +2,14 @@ const clock = document.querySelector("h2#clock");
 
 function getclock(){
     const date = new Date();
-    // console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
-    clock.innerText =`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`
 }
 
 //"새로고침 한 직후에 일단 한번 출력"한 후, 1000ms마다 소환하자.
 getclock();
  setInterval(getclock, 1000);
 // setTimeout(sayHello, 5000);
+  
